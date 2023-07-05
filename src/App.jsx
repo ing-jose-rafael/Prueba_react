77ecdb27367b9df5c6a795584418ed0a90e03ec8
josecarreno <ing.jcarreno@gmail.com>
2023-07-05 14:05:34 -0500

    Refactorizando el App.jsx utilizando el componente ListOfCards

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,15 @@
 import './App.css'
-import { cards } from './mooks/cards'
+import { ListOfCards } from './components/ListOfCards'
 
 function App () {
   return (
     <div className='page'>
-    <header>
-      <h1>Prueba técnica</h1>
-    </header>
-      <ul>
-        {
-          cards.map(card => {
-            return (
-              <li key={card.id}>
-                <div className="card-icon" dangerouslySetInnerHTML={{ __html: card.icono }}></div>
-                {/* {card.icono.split('').slice(1, -1).join('')} */}
-                <h3>{card.title}</h3>
-                <p>{card.description}</p>
-                <a>Ver más...</a>
-              </li>
-            )
-          }
-          )
-        }
-      </ul>
+      <header>
+        <h1>Prueba técnica</h1>
+      </header>
+      <main>
+        <ListOfCards/>
+      </main>
     </div>
   )
 }
