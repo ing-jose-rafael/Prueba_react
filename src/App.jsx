212603a2c9c56e54962bea240d549b325a8b8cf5
josecarreno <ing.jcarreno@gmail.com>
2023-07-05 13:12:26 -0500

    primer diseño de CONTENT

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
 import './App.css'
+import { cards } from './mooks/cards'
 
 function App () {
   return (
-    <>
-
-    </>
+    <div className='page'>
+    <header>
+      <h1>Prueba técnica</h1>
+    </header>
+      <ul>
+        {
+          cards.map(card => {
+            return (
+              <li key={card.id}>
+                <div className="card-icon" dangerouslySetInnerHTML={{ __html: card.icono }}></div>
+                {/* {card.icono.split('').slice(1, -1).join('')} */}
+                <h3>{card.title}</h3>
+                <p>{card.description}</p>
+                <a>Ver más...</a>
+              </li>
+            )
+          }
+          )
+        }
+      </ul>
+    </div>
   )
 }
 
