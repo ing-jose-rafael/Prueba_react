b33a950352f49ac24edb1a5b6e2fd4abb227d50c
josecarreno <ing.jcarreno@gmail.com>
2023-07-06 11:48:08 -0500

    Mejorando el componente Card.jsx y creando los estilos

--- src/components/Crad.jsx
+++ src/components/Crad.jsx
@@ -1,12 +1,21 @@
-export const Crad = ({ card }) => {
-  console.log(card)
+import './Card.css'
+
+// #249eff #ff1f6b
+export function Crad ({ card }) {
   return (
-    <>
-    <div className="card-icon" dangerouslySetInnerHTML={{ __html: card.icono }}></div>
-    {/* {card.icono.split('').slice(1, -1).join('')} */}
-    <h3>{card.title}</h3>
-    <p>{card.description}</p>
-    <a>Ver más...</a>
-    </>
+    <div className="container">
+      <div className="card" style={{ '--clr': '#98ff21' }}>
+        <div className="box">
+          <div className="icon">
+            <div className="iconBox" dangerouslySetInnerHTML={{ __html: card.icono }}></div>
+          </div>
+          <div className="content">
+            <h3>{card.title}</h3>
+            <p>{card.description}</p>
+            <a>Ver más...</a>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
