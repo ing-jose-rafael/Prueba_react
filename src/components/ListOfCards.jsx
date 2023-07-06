b8b6ce90dea5dd23107e263a6ab84e49aec3e885
josecarreno <ing.jcarreno@gmail.com>
2023-07-06 11:49:35 -0500

    agregando la clase card al elemento li

--- src/components/ListOfCards.jsx
+++ src/components/ListOfCards.jsx
@@ -3,12 +3,13 @@ import { Crad } from './Crad'
 export const ListOfCards = () => {
   return (
     <>
+    {/* <Crad2/> */}
     <ul>
         {
           cards.map(card => {
             return (
-              <li key={card.id}>
-                <Crad card={card} />
+              <li key={card.id} className='card'>
+                <Crad card={card}/>
               </li>
             )
           }
