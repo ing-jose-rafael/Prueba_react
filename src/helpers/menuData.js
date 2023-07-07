8c5eb613133f61e849e39f23eda8da6fc93ef6d9
josecarreno <ing.jcarreno@gmail.com>
2023-07-07 15:58:03 -0500

    creando un arreglo con las oopciones del menu MenuData

---
+++ src/helpers/menuData.js
@@ -0,0 +1,20 @@
+export const MenuData = [
+  {
+    title: 'Home',
+    link: '/',
+    cName: 'nav-links',
+    icon: 'fas fa-house-user'
+  },
+  {
+    title: 'Projects',
+    link: '/projects',
+    cName: 'nav-links',
+    icon: 'fas fa-briefcase'
+  },
+  {
+    title: 'About',
+    link: '/about',
+    cName: 'nav-links',
+    icon: 'fas fa-info-circle'
+  }
+]
