48d8a4208a858fec95911b7be0e81a4e32f44f8b
josecarreno <ing.jcarreno@gmail.com>
2023-07-07 12:32:28 -0500

    creando el Navbar

---
+++ src/components/Navar.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+import { MenuData } from '../helpers/menuData'
+import './NavbarStyles.css'
+export const Navbar = () => {
+  const [isToggle, setIsToggle] = useState(false)
+  return (
+    <nav className='navbar'>
+      <h1 className='logo'>
+        Jose <i className='fab fa-react'></i>
+      </h1>
+      <div className='toggle'>
+        <i className={isToggle ? 'fas fa-times' : 'fas fa-bars'} onClick={() => setIsToggle(!isToggle)}></i>
+      </div>
+      <ul className={isToggle ? 'nav-menu active' : 'nav-menu'}>
+        {
+          MenuData.map((item, index) => {
+            return (
+              <li key={index}>
+                <a href={item.link} className={item.cName}><i className={item.icon}></i>{item.title}</a>
+              </li>
+            )
+          })
+        }
+      </ul>
+    </nav>
+  )
+}
