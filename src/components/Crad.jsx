59f5114b4a34f8670c7bc1fc67e3bd38c6c1422c
josecarreno <ing.jcarreno@gmail.com>
2023-07-11 15:19:40 -0500

    refactorizando el codigo en el componente Crad y utilizando el hook useCrads para utilizar la funsion addToggleFavorited

--- src/components/Crad.jsx
+++ src/components/Crad.jsx
@@ -1,18 +1,42 @@
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
 import './Card.css'
+import { CardContext } from '../context/cardContext'
+import { useCrads } from '../hooks/useCrads'
+// import { favorite } from '../helpers/getDetailCard'
 
 // #249eff #ff1f6b
-export function Crad ({ card }) {
+export function Crad ({ card, viewMore = false }) {
+  const [isFavorite, setIsFavorite] = useState(card.favore)
+  const { addToggleFavorited } = useCrads(CardContext)
+
+  const text = viewMore ? 'Regesar...' : 'Ver más...'
+  const path = viewMore ? '/' : `/cards/${card.id}`
+  const cardColor = isFavorite ? '#ff1f6b' : '#98ff21'
+  const cardIconClass = isFavorite ? 'fa-solid fa-heart' : 'fa-regular fa-heart'
+
+  const handleFav = () => {
+    // favorite(card.id)
+    setIsFavorite(!isFavorite)
+    addToggleFavorited(card.id)
+  }
+
   return (
     <div className="container">
-      <div className="card" style={{ '--clr': '#98ff21' }}>
+      <div className="card" style={{ '--clr': cardColor }}>
         <div className="box">
           <div className="icon">
-            <div className="iconBox" dangerouslySetInnerHTML={{ __html: card.icono }}></div>
+            <div className="iconBox">
+              <button className='btn' onClick={handleFav}>
+              <i className={cardIconClass}></i>
+              </button>
+            </div>
+            {/* <div className="iconBox" dangerouslySetInnerHTML={{ __html: card.icono }}></div> */}
           </div>
           <div className="content">
             <h3>{card.title}</h3>
             <p>{card.description}</p>
-            <a>Ver más...</a>
+            <Link to={path}>{text}</Link>
           </div>
         </div>
       </div>
