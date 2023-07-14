e33cdde911e56ca23052a54c8b8d1aecbbef3a7d
josecarreno <ing.jcarreno@gmail.com>
2023-07-13 23:25:16 -0500

    creando el Mooks

---
+++ src/mooks/deports.js
@@ -0,0 +1,357 @@
+export const category = [
+  {
+    id: 1,
+    name: 'Motor',
+    // icon: 'https://www.svgrepo.com/show/490925/basketball.svg'
+    icon: 'https://www.svgrepo.com/show/480595/race-car-1.svg'
+  },
+  {
+    id: 2,
+    name: 'Mesa',
+    icon: 'https://www.svgrepo.com/show/480345/tennis-2.svg'
+  },
+  {
+    id: 3,
+    name: 'Equipo',
+    icon: 'https://www.svgrepo.com/show/480342/basketball-goal-4.svg'
+  }
+]
+
+export const deport = [
+  {
+    id: 1,
+    name: 'Futbol',
+    video: 'https://v4.cdnpk.net/videvo_files/video/free/video0478/large_watermarked/_import_62735e1cd64e41.23043471_FPpreview.mp4',
+    description: 'lorem ipsum dolor sit amet',
+    categoriId: 3,
+    links: [
+      {
+        name: 'Liverpool',
+        url: 'https://www.liverpoolfc.com/'
+      },
+      {
+        name: 'Mancity',
+        url: 'https://es.mancity.com/'
+      },
+      {
+        name: 'Realmadrid',
+        url: 'https://www.realmadrid.com/'
+      }
+    ]
+  },
+  {
+    id: 2,
+    name: 'Deporte 2',
+    video: 'https://v1.cdnpk.net/videvo_files/video/free/video0542/large_watermarked/_import_6319825037ef70.75899118_FPpreview.mp4',
+    description: 'lorem ipsum dolor sit amet',
+    categoriId: 2,
+    links: [
+      {
+        name: 'Liverpool',
+        url: 'https://www.liverpoolfc.com/'
+      },
+      {
+        name: 'Mancity',
+        url: 'https://es.mancity.com/'
+      },
+      {
+        name: 'Realmadrid',
+        url: 'https://www.realmadrid.com/'
+      }
+    ]
+  },
+  {
+    id: 3,
+    name: 'Baloncesto',
+    video: 'https://v4.cdnpk.net/videvo_files/video/free/video0465/large_watermarked/_import_613ef587512cd7.44309882_FPpreview.mp4',
+    description: 'lorem ipsum dolor sit amet',
+    categoriId: 3,
+    links: [
+      {
+        name: 'Liverpool',
+        url: 'https://www.liverpoolfc.com/'
+      },
+      {
+        name: 'Mancity',
+        url: 'https://es.mancity.com/'
+      },
+      {
+        name: 'Realmadrid',
+        url: 'https://www.realmadrid.com/'
+      }
+    ]
+  },
+  {
+    id: 4,
+    name: 'Deporte 4',
+    video: 'https://v4.cdnpk.net/videvo_files/video/free/video0465/large_watermarked/_import_613f08b1c2c3a6.07345434_FPpreview.mp4',
+    description: 'lorem ipsum dolor sit amet',
+    categoriId: 1,
+    links: [
+      {
+        name: 'Liverpool',
+        url: 'https://www.liverpoolfc.com/'
+      },
+      {
+        name: 'Mancity',
+        url: 'https://es.mancity.com/'
+      },
+      {
+        name: 'Realmadrid',
+        url: 'https://www.realmadrid.com/'
+      }
+    ]
+  },
+  {
+    id: 5,
+    name: 'Deporte 5',
+    video: 'https://v4.cdnpk.net/videvo_files/video/free/video0469/large_watermarked/_import_6172ac5df24c14.50412847_FPpreview.mp4',
+    description: 'lorem ipsum dolor sit amet',
+    categoriId: 1,
+    links: [
+      {
+        name: 'Liverpool',
+        url: 'https://www.liverpoolfc.com/'
+      },
+      {
+        name: 'Mancity',
+        url: 'https://es.mancity.com/'
+      },
+      {
+        name: 'Realmadrid',
+        url: 'https://www.realmadrid.com/'
+      }
+    ]
+  },
+  {
+    id: 6,
+    name: 'Deporte 6',
+    video: 'https://v4.cdnpk.net/videvo_files/video/free/video0458/large_watermarked/_import_60c450853005c6.70505096_FPpreview.mp4',
+    description: 'lorem ipsum dolor sit amet',
+    categoriId: 1,
+    links: [
+      {
+        name: 'Liverpool',
+        url: 'https://www.liverpoolfc.com/'
+      },
+      {
+        name: 'Mancity',
+        url: 'https://es.mancity.com/'
+      },
+      {
+        name: 'Realmadrid',
+        url: 'https://www.realmadrid.com/'
+      }
+    ]
+  },
+  {
+    id: 7,
+    name: 'Deporte 7',
+    video: 'https://v4.cdnpk.net/videvo_files/video/free/video0478/large_watermarked/_import_6275fe4bdd38a3.70475221_FPpreview.mp4',
+    description: 'lorem ipsum dolor sit amet',
+    categoriId: 1,
+    links: [
+      {
+        name: 'Liverpool',
+        url: 'https://www.liverpoolfc.com/'
+      },
+      {
+        name: 'Mancity',
+        url: 'https://es.mancity.com/'
+      },
+      {
+        name: 'Realmadrid',
+        url: 'https://www.realmadrid.com/'
+      }
+    ]
+  },
+  {
+    id: 8,
+    name: 'Deporte 8',
+    video: 'https://v4.cdnpk.net/videvo_files/video/free/video0478/large_watermarked/_import_627a0ae6594286.40649948_FPpreview.mp4',
+    description: 'lorem ipsum dolor sit amet',
+    categoriId: 2,
+    links: [
+      {
+        name: 'Liverpool',
+        url: 'https://www.liverpoolfc.com/'
+      },
+      {
+        name: 'Mancity',
+        url: 'https://es.mancity.com/'
+      },
+      {
+        name: 'Realmadrid',
+        url: 'https://www.realmadrid.com/'
+      }
+    ]
+  },
+  {
+    id: 19,
+    name: 'Deporte 19',
+    video: 'https://v4.cdnpk.net/videvo_files/video/free/video0458/large_watermarked/_import_60c45062f1c687.64956751_FPpreview.mp4',
+    description: 'lorem ipsum dolor sit amet',
+    categoriId: 2,
+    links: [
+      {
+        name: 'Liverpool',
+        url: 'https://www.liverpoolfc.com/'
+      },
+      {
+        name: 'Mancity',
+        url: 'https://es.mancity.com/'
+      },
+      {
+        name: 'Realmadrid',
+        url: 'https://www.realmadrid.com/'
+      }
+    ]
+  },
+  {
+    id: 18,
+    name: 'Deporte 18',
+    video: 'https://v4.cdnpk.net/videvo_files/video/free/video0477/large_watermarked/_import_6257aeb9532837.27413918_FPpreview.mp4',
+    description: 'lorem ipsum dolor sit amet',
+    categoriId: 2,
+    links: [
+      {
+        name: 'Liverpool',
+        url: 'https://www.liverpoolfc.com/'
+      },
+      {
+        name: 'Mancity',
+        url: 'https://es.mancity.com/'
+      },
+      {
+        name: 'Realmadrid',
+        url: 'https://www.realmadrid.com/'
+      }
+    ]
+  },
+  {
+    id: 17,
+    name: 'Deporte 17',
+    video: 'https://v4.cdnpk.net/videvo_files/video/free/video0478/large_watermarked/_import_62735b10005565.42296056_FPpreview.mp4',
+    description: 'lorem ipsum dolor sit amet',
+    categoriId: 2,
+    links: [
+      {
+        name: 'Liverpool',
+        url: 'https://www.liverpoolfc.com/'
+      },
+      {
+        name: 'Mancity',
+        url: 'https://es.mancity.com/'
+      },
+      {
+        name: 'Realmadrid',
+        url: 'https://www.realmadrid.com/'
+      }
+    ]
+  },
+  {
+    id: 21,
+    name: 'Deporte 21',
+    video: 'https://v4.cdnpk.net/videvo_files/video/free/video0457/large_watermarked/_import_60bc652b27abc5.87101361_FPpreview.mp4',
+    description: 'lorem ipsum dolor sit amet',
+    categoriId: 3,
+    links: [
+      {
+        name: 'Liverpool',
+        url: 'https://www.liverpoolfc.com/'
+      },
+      {
+        name: 'Mancity',
+        url: 'https://es.mancity.com/'
+      },
+      {
+        name: 'Realmadrid',
+        url: 'https://www.realmadrid.com/'
+      }
+    ]
+  },
+  {
+    id: 10,
+    name: 'Deporte 10',
+    video: 'https://v4.cdnpk.net/videvo_files/video/free/video0478/large_watermarked/_import_627a0a08615293.79138890_FPpreview.mp4',
+    description: 'lorem ipsum dolor sit amet',
+    categoriId: 3,
+    links: [
+      {
+        name: 'Liverpool',
+        url: 'https://www.liverpoolfc.com/'
+      },
+      {
+        name: 'Mancity',
+        url: 'https://es.mancity.com/'
+      },
+      {
+        name: 'Realmadrid',
+        url: 'https://www.realmadrid.com/'
+      }
+    ]
+  },
+  {
+    id: 11,
+    name: 'Deporte 11',
+    video: 'https://v1.cdnpk.net/videvo_files/video/free/video0542/large_watermarked/_import_631984c0d2c326.67771485_FPpreview.mp4',
+    description: 'lorem ipsum dolor sit amet',
+    categoriId: 3,
+    links: [
+      {
+        name: 'Liverpool',
+        url: 'https://www.liverpoolfc.com/'
+      },
+      {
+        name: 'Mancity',
+        url: 'https://es.mancity.com/'
+      },
+      {
+        name: 'Realmadrid',
+        url: 'https://www.realmadrid.com/'
+      }
+    ]
+  },
+  {
+    id: 13,
+    name: 'Deporte 13',
+    video: 'https://v4.cdnpk.net/videvo_files/video/free/video0467/large_watermarked/_import_615579a59b5687.27049363_FPpreview.mp4',
+    description: 'lorem ipsum dolor sit amet',
+    categoriId: 3,
+    links: [
+      {
+        name: 'Liverpool',
+        url: 'https://www.liverpoolfc.com/'
+      },
+      {
+        name: 'Mancity',
+        url: 'https://es.mancity.com/'
+      },
+      {
+        name: 'Realmadrid',
+        url: 'https://www.realmadrid.com/'
+      }
+    ]
+  },
+  {
+    id: 14,
+    name: 'Deporte 14',
+    video: 'https://v4.cdnpk.net/videvo_files/video/free/video0458/large_watermarked/_import_60c44ffa7d6c09.66091746_FPpreview.mp4',
+    description: 'lorem ipsum dolor sit amet',
+    categoriId: 3,
+    links: [
+      {
+        name: 'Liverpool',
+        url: 'https://www.liverpoolfc.com/'
+      },
+      {
+        name: 'Mancity',
+        url: 'https://es.mancity.com/'
+      },
+      {
+        name: 'Realmadrid',
+        url: 'https://www.realmadrid.com/'
+      }
+    ]
+  }
+]
