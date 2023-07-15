6c9e236faea161bd91f465e175734468334c75e5
josecarreno <ing.jcarreno@gmail.com>
2023-07-14 21:18:36 -0500

    utilizando el hooks useDeports en el componente Create

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -1,13 +1,31 @@
 import './CreateStyle.css'
+import { useDeports } from '../hooks/useDeports'
+import { useState } from 'react'
 import { category } from '../mooks/deports'
 
 export const Create = () => {
+  const { createDeport } = useDeports()
+  const [categorySelected, setCategory] = useState(1)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const form = e.target
+    const formData = new FormData(form)
+
+    const { name, description } = Object.fromEntries(formData.entries())
+    createDeport({ name, description, categoriId: categorySelected })
+    form.reset()
+  }
+
+  const handleChange = (e) => {
+    setCategory(+e.target.value)
+  }
   return (
     <div className="create">
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="inputSelect">
           <label>Categoría</label>
-          <select>
+          <select onChange={handleChange}>
             {
               category.map(gener => (
                 <option key={gener.id} value={gener.id}>{gener.name}</option>
@@ -17,14 +35,14 @@ export const Create = () => {
 
         </div>
         <div className="inputBox">
-          <input type="text" required/>
+          <input type="text" name='name' required/>
           <span>Deporte</span>
         </div>
         <div className="inputBox">
-          <input type="text" required/>
+          <input type="text" name='description' required/>
           <span>Descipción</span>
         </div>
-        <button className='btn1'><span>Guadar</span><i></i></button>
+        <button className='btn1' type='submit'><span>Guadar</span><i></i></button>
       </form>
     </div>
   )
